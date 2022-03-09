7ecefd22db10db929a0a4bc26e9c4a77530ed2a7
Tiago Colli <tiago.plakitca@magnetis.com.br>
2022-03-09 12:54:49 -0300

    test: add missing schema test

--- test/github_service/repos/contributor_test.exs
+++ test/github_service/repos/contributor_test.exs
@@ -9,40 +9,26 @@ defmodule GithubService.Repos.ContributorTest do
   describe "changeset/2" do
     test "when the given params are valid, returns a valid changeset" do
       new_contributor = %{
-        github_id: 123,
         name: Lorem.word(),
         qtd_commits: 10,
-        user: Lorem.word(),
-        repository_id: 123
+        user: Lorem.word()
       }
 
       assert %{valid?: true} = Contributor.changeset(%Contributor{}, new_contributor)
     end
 
-    test "when the given params are not valid, raises an exception" do
-      new_contributor = %{}
-
-      assert_raise FunctionClauseError, fn ->
-        Contributor.changeset(new_contributor, new_contributor)
-      end
-    end
-
     test "when the attrs type are not valid, returns an invalid changeset with the errors" do
       new_contributor = %{
-        github_id: Lorem.word(),
         name: 123,
         qtd_commits: Lorem.word(),
-        user: 123,
-        repository_id: Lorem.word()
+        user: 123
       }
 
       assert %{valid?: false} = changeset = Contributor.changeset(%Contributor{}, new_contributor)
 
-      assert "is invalid" in errors_on(changeset).github_id
       assert "is invalid" in errors_on(changeset).name
       assert "is invalid" in errors_on(changeset).qtd_commits
       assert "is invalid" in errors_on(changeset).user
-      assert "is invalid" in errors_on(changeset).repository_id
     end
 
     test "when required attrs are not filled, returns an invalid changeset with the errors" do
@@ -51,11 +37,9 @@ defmodule GithubService.Repos.ContributorTest do
       assert %{valid?: false} = changeset = Contributor.changeset(%Contributor{}, new_contributor)
 
       assert errors_on(changeset) == %{
-               github_id: ["can't be blank"],
                name: ["can't be blank"],
                qtd_commits: ["can't be blank"],
-               user: ["can't be blank"],
-               repository_id: ["can't be blank"]
+               user: ["can't be blank"]
              }
     end
   end
