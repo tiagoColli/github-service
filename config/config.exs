15df5d9b801ffc3a1afc146bcae7bdbe77e96e2b
Tiago Colli <tiago.plakitca@magnetis.com.br>
2022-03-09 12:30:57 -0300

    feat: adding api base urls into enviroment variables

--- config/config.exs
+++ config/config.exs
@@ -31,6 +31,11 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Github base api URL
+config :github_service,
+  github_base_api: "https://api.github.com",
+  webhook_site_url: "https://webhook.site"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
